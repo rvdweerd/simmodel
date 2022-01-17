1d026b482f07c6ed97a1e881f5de05c9d6cf1bfa
rvdweerd <rogerscode@gmail.com>
2022-01-17 13:54:00 +0100

    edge removal improved

--- modules/sim/graph_factory.py
+++ modules/sim/graph_factory.py
@@ -1,29 +1,82 @@
 import numpy as np
 from itertools import product
+import random 
 
-def all_Manhattan3x3_symmetric_adj_matrices(reflexive=True):
-    n=3
-    N=n**2
-    K=(N-n)*2
+def rand_key(p):
+    key1 = ""
+    for i in range(p):
+        temp = str(random.randint(0, 1))
+        key1 += temp
+    return(key1)
+
+def rand_key_fixed_num_removed(K, num_removed):
+    arr=np.array([0]*num_removed + [1]*(K-num_removed))
+    np.random.shuffle(arr)
+    return arr
+
+def create_adj_matrix(N,coord_upper,vals,W_):
+    arr = np.zeros((N,N))
+    #i_up=np.array([(0,1),(0,3),(1,2),(1,4),(2,5),(3,4),(3,6),(4,5),(4,7),(5,8),(6,7),(7,8)])
+    i_y=np.array([i for (i,j) in coord_upper]) # index the elements in the upper triangular to be set
+    i_x=np.array([j for (i,j) in coord_upper])        
+    arr[i_y,i_x]=vals
+    arr += arr.T # make symmetric
+    arr[np.diag_indices_from(arr)] = np.diag(W_) # copy the diagonal elements
+    return arr
+
+def get_all_edge_removals_symmetric(W_, removals=[2,3,4], instances_per_num_removed=10):
+    ##
+    # params:
+    # W_ : adjacency matrix (numpy array)
+    # removals: list with number of edges to be removed
+    # instances_per_num_removed: how many graphs created for each case
+    #
+    # returns:
+    # all_W                     : list containing all combinations of edges removed as tuple (W_reduced, num_edges_reduced, hash)
+    # W_per_num_edge_removals   : dict from number of edges removed to list of tuples (W_reduced, hash)  
+    assert np.array_equal(W_.T,W_) # symmetric
+    N=W_.shape[0] # number of nodes
+    #n=np.sqrt(N)
+    idx_upper = np.triu_indices(N, k=1) # offset k=1 (excluding diagonal entries)
+    coord_upper = [(i,j) for i,j in zip(idx_upper[0],idx_upper[1]) if W_[i,j]>0] # the edge pool up for removal
+    K=len(coord_upper) # the number of edges in the edge pool up for removal (K=(N-n)*2 for Manhattan graph)
+
+    # Containers
     all_W = []
-    W_per_num_edge_removals={i:[] for i in range(K+1)}
-    hashes=set()
-    for vals in product([0, 1], repeat=(K)):
-        hash = int("".join(str(v) for v in vals), 2)
-        if hash in hashes:
-            assert False
-        else:
-            hashes.add(hash)
-        arr = np.zeros((N,N))
-        i_up=np.array([(0,1),(0,3),(1,2),(1,4),(2,5),(3,4),(3,6),(4,5),(4,7),(5,8),(6,7),(7,8)])
-        i_y=np.array([i for (i,j) in i_up])
-        i_x=np.array([j for (i,j) in i_up])        
-        arr[i_y,i_x]=vals
-        arr=arr+arr.T
-        if reflexive:
-            arr=arr+np.eye(N)
-        # Check; no orphan nodes (nodes with no edges)
-        if np.min(arr.sum(axis=1)) > 1:
-            all_W.append(arr)
-            W_per_num_edge_removals[K-np.sum(vals)].append(arr)
+    hashes_int=set()
+    if K <= 12: # manageable number of permutation, 2^12=4096, return exhaustive list
+        W_per_num_edge_removals={i:[] for i in range(K+1)}
+        for vals in product([0, 1], repeat=(K)):
+            hash_str = "".join(str(v) for v in vals)
+            hash_int = int("".join(str(v) for v in vals), 2)
+            if hash_int in hashes_int:
+                assert False
+            else:
+                hashes_int.add(hash)
+            arr = create_adj_matrix(N,coord_upper,vals,W_)
+            # Check; no orphan nodes (nodes with no edges)
+            if np.min(arr.sum(axis=1)) > 1:
+                num_edges_removed = K-np.sum(vals)
+                all_W.append((arr, num_edges_removed, hash_int))
+                W_per_num_edge_removals[K-np.sum(vals)].append((arr, hash_int))
+    else:
+        #assert instances_per_num_removed <= K
+        W_per_num_edge_removals={i:[] for i in removals}
+        for num_removed in removals:#range(1,K//2-1):
+            #theoretic_max = np.prod(range(K-num_removed+1,K+1))
+            while len(W_per_num_edge_removals[num_removed]) < instances_per_num_removed:# and len(W_per_num_edge_removals[num_removed]) != :
+                vals = rand_key_fixed_num_removed(K, num_removed)
+                hash_str = "".join(str(v) for v in vals)
+                hash_int = int(hash_str, 2)
+                if hash_int in hashes_int:
+                    assert False
+                else:
+                    hashes_int.add(hash)
+                arr = create_adj_matrix(N,coord_upper,vals,W_)
+                # Check; no orphan nodes (nodes with no edges)
+                if np.min(arr.sum(axis=1)) > 1:
+                    num_edges_removed = K-np.sum(vals)
+                    all_W.append((arr, num_edges_removed, hash_int))
+                    W_per_num_edge_removals[K-np.sum(vals)].append((arr, hash_int))
+
     return all_W, W_per_num_edge_removals
--- test_graph_factory.py
+++ test_graph_factory.py
@@ -1,5 +1,6 @@
-from modules.sim.graph_factory import all_Manhattan3x3_symmetric_adj_matrices
+from modules.sim.graph_factory import all_Manhattan3x3_symmetric_adj_matrices, get_all_edge_removals_symmetric
 from modules.rl.rl_custom_worlds import GetCustomWorld
+from modules.sim.simdata_utils import SimulateInteractiveMode
 from modules.rl.environments import GraphWorld
 import networkx as nx
 
@@ -17,20 +18,19 @@ config={
 
 state_repr='et'
 state_enc='nodes'
-env = GraphWorld(config, optimization_method='static', fixed_initial_positions=None, state_representation=state_repr, state_encoding=state_enc)
+#env = GraphWorld(config, optimization_method='static', fixed_initial_positions=None, state_representation=state_repr, state_encoding=state_enc)
 
-# https://networkx.org/documentation/networkx-1.7/reference/generated/networkx.convert.from_numpy_matrix.html
-# G=nx.from_numpy_matrix(A)
-# networkx.relabel_nodes
-#world_name='Manhattan5x5_FixedEscapeInit'
-#env = GetCustomWorld(world_name, make_reflexive=True, state_repr=state_repr, state_enc='nodes')
-W_all, W_per_num_edge_removals=all_Manhattan3x3_symmetric_adj_matrices()
+#world_name='MetroU3_e17tborder_FixedEscapeInit'
+world_name='Manhattan5x5_FixedEscapeInit'
+env = GetCustomWorld(world_name, make_reflexive=True, state_repr=state_repr, state_enc='nodes')
+W_all, W_per_num_edge_removals = get_all_edge_removals_symmetric(nx.convert_matrix.to_numpy_matrix(env.sp.G),removals=[4,8,12,16],instances_per_num_removed=10)
 
 for k,v in W_per_num_edge_removals.items():
+    # v: list of tuples
     print(k,len(v))
 
-for i in range(4):#len(W_per_num_edge_removals[2])):
-    W=W_per_num_edge_removals[3][i]
+for k,v in W_per_num_edge_removals.items():#len(W_per_num_edge_removals[2])):
+    W=v[0][0]
     env.redefine_graph_structure(W,env.sp.nodeid2coord)
-    env.render(mode=None, fname='graph_'+str(i))
-k=0
+    env.render(mode=None, fname='graph_'+str(k))
+    SimulateInteractiveMode(env)
