fda98a841be722a82a30f5d312c890da3fe93936
rvdweerd <rogerscode@gmail.com>
2022-02-10 11:33:07 +0100

    initiate ppo spinning_up

--- Phase2d_experiments_pfinding_gnn-ppo.py
+++ Phase2d_experiments_pfinding_gnn-ppo.py
@@ -30,7 +30,7 @@ class PPONet(nn.Module):
         self.eps_clip       = config['eps_clip']   
         self.K_epoch        = config['K_epoch']     
         self.num_epi        = config['num_epi']     
-
+        self.count=0
         self.data = []
 
         # Build the learnable affine maps:
@@ -40,10 +40,14 @@ class PPONet(nn.Module):
         self.theta3 = nn.Linear(self.emb_dim, self.emb_dim, True, dtype=torch.float32)
         self.theta4 = nn.Linear(1, self.emb_dim, True, dtype=torch.float32)
         self.theta5_pi = nn.Linear(2*self.emb_dim, 1, True, dtype=torch.float32)
-        self.theta5_v  = nn.Linear(2*self.emb_dim, 1, True, dtype=torch.float32) # Maybe too complex, perhaps share weights with th_5_pi CHECK / TODO
+        #self.theta5_pi2 = nn.Linear(self.emb_dim, 1, True, dtype=torch.float32) # Maybe too complex, perhaps share weights with th_5_pi CHECK / TODO 
+        self.theta5_v1 = nn.Linear(2*self.emb_dim, 1, True, dtype=torch.float32)
+        #self.theta5_v2  = nn.Linear(self.emb_dim, 1, True, dtype=torch.float32) # Maybe too complex, perhaps share weights with th_5_pi CHECK / TODO
         self.theta6 = nn.Linear(self.emb_dim, self.emb_dim, True, dtype=torch.float32)
         self.theta7 = nn.Linear(self.emb_dim, self.emb_dim, True, dtype=torch.float32)
   
+        #self.thetav1 = nn.Linear(self.emb_dim*2,self.emb_dim, True, dtype=torch.float32)
+        #self.thetav2 = nn.Linear(self.emb_dim, 1, True, dtype=torch.float32)
         #self.numTrainableParameters()
         self.optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
 
@@ -81,25 +85,40 @@ class PPONet(nn.Module):
         global_state = self.theta6(torch.sum(mu, dim=1, keepdim=True).repeat(1, num_nodes, 1))
         local_action = self.theta7(mu)  # (batch_dim, nr_nodes, emb_dim)
         out = F.relu(torch.cat([global_state, local_action], dim=2)) # concat creates (batch_dim, nr_nodes, 2*emb_dim)
-        return out
+        return out 
 
     def pi(self, x, W, reachable_nodes):
         # reachable_nodes (batch_dim, nr_nodes)
         reachable_nodes=reachable_nodes.type(torch.BoolTensor)
 
         rep = self.propagate(x, W) # (batch_dim, nr_nodes, 2*emb_dim)
+        #rep = F.relu(self.theta5_pi1(rep))
         prob_logits = self.theta5_pi(rep).squeeze(dim=2) # (batch_dim, nr_nodes)
 
         # mask invalid actions
         prob_logits[~reachable_nodes] = -torch.inf
+        #print(self.count,prob_logits)
+        self.count+=1
         return prob_logits # returns the logits!!! (batch_dim, nr_nodes)
+
     
     def v(self, x, W, reachable_nodes):
         rep = self.propagate(x, W) # (batch_dim, nr_nodes, 2*emb_dim)
-        rep2 = self.theta5_pi(rep).squeeze(dim=2) # (batch_dim, nr_nodes)
-        #reachable_nodes=reachable_nodes.type(torch.BoolTensor)
-        #rep2[~reachable_nodes] = 0
-        v = rep2.mean(dim=1)
+        
+        # # OPTION 1: aggregate (mean = best) and project
+        # #rep2 = torch.max(rep,dim=1)[0]
+        # rep = rep.mean(dim=1)
+        # v = self.theta5_v1(rep).squeeze()
+
+        # OPTION 2: mimic Q-function and take the max in the reachable node dimension
+        #rep = F.relu(self.theta5_v1(rep))
+        rep = self.theta5_v1(rep).squeeze(dim=2) # (batch_dim, nr_nodes)
+        # optional: ONLY LOOK AT DIRECT NEIGHBORHOOD
+        #reachable_nodes=reachable_nodes.type(torch.BoolTensor).to(device)
+        #rep[~reachable_nodes] = -torch.inf
+        #v=torch.max(rep,dim=1)[0]
+        #v = (rep * reachable_nodes).mean(dim=1)
+        v = rep.mean(dim=1)
         return v # (batch_dim): a value of the current graph state for each transition
       
     def put_data(self, transition):
@@ -128,7 +147,7 @@ class PPONet(nn.Module):
         self.data = []
         return s, a, r, s_prime, done_mask, prob_a, W, reachable_nodes
         
-    def train_net(self):
+    def train_net(self, writer, it):
         s, a, r, s_prime, done_mask, prob_a, W, reachable_nodes = self.make_batch()
         a=a.to(device)
         r=r.to(device)
@@ -158,6 +177,12 @@ class PPONet(nn.Module):
 
             self.optimizer.zero_grad()
             loss.mean().backward()
+            
+            parameters = self.theta1a.parameters()
+            parameters_norm=torch.norm(torch.stack([torch.norm(p.grad.detach(),2) for p in parameters]), 2)
+            writer.add_scalar('theta1a',parameters_norm,it)
+            
+            
             self.optimizer.step()
         return loss.mean().detach().cpu()
         
@@ -167,9 +192,10 @@ def GetTrainedModel(env, config, logdir='runs/test'):
     writer = SummaryWriter(logdir)
     model = PPONet(config).to(device)
     #writer.add_graph(model)
-    score = 0.0
-    print_interval = 20
-    plot_interval=250
+    score = 0.
+    lossadd = 0.
+    print_interval = config['print_interval']
+    plot_interval=config['plot_interval']
 
     for n_epi in range(num_epi):
         env.reset()
@@ -189,21 +215,46 @@ def GetTrainedModel(env, config, logdir='runs/test'):
 
             score += r
 
-        loss = model.train_net()
+        loss = model.train_net(writer, n_epi)
+        lossadd+=loss
 
-        writer.add_scalar('loss',loss,n_epi)
-        writer.add_scalar('return per epi',score,n_epi)
-        if n_epi%print_interval==0 and n_epi!=0:
-            print("# of episode :{}, avg score : {:.1f}".format(n_epi, score/print_interval))
-            score = 0.0
         if n_epi%plot_interval==0 and n_epi!=0:
             env.render_eupaths(fname='./'+logdir+'/rendering_active',t_suffix=False)
+        if n_epi%print_interval==0 and n_epi!=0:
+            p1,p2 = get_test_probs(env,model)
+            
+
+            print("# of episode :{}, avg score : {:.1f}".format(n_epi, score/print_interval))
+            print(p1)
+            print(p2)
+            writer.add_scalar('return per epi',score/print_interval,n_epi)
+            writer.add_scalar('loss',loss/print_interval,n_epi)
+            score = 0.
+            lossadd=0.
 
     env.close()
 
+def get_test_probs(env, model):
+    env.reset()
+    done=False
+    probs=[]
+    consecutive_action_queries=[3,4]
+    for a in consecutive_action_queries:
+        s = env.nfm
+        with torch.no_grad():
+            model.eval()
+            prob_logits = model.pi(s.unsqueeze(0), env.sp.W.unsqueeze(0), env.sp.W[env.state[0]].unsqueeze(0))
+            model.train()
+            pl_select = prob_logits > -torch.inf
+            m = Categorical(logits = prob_logits[pl_select].squeeze())
+        probs.append(m.probs.detach().cpu())
+        _,_,done,_ = env.step(a)
+    return tuple(probs)
+
+
 if __name__ == '__main__':
-    world_name='MetroU3_e17tborder_FixedEscapeInit'
-    #world_name='Manhattan3x3_WalkAround'
+    #world_name='MetroU3_e17tborder_FixedEscapeInit'
+    world_name='Manhattan3x3_WalkAround'
     
     #scenario_name='TrainMetro'
     state_repr='etUte0U0'
@@ -218,29 +269,34 @@ if __name__ == '__main__':
     edge_blocking = True
     nfm_func_name = 'NFM_ev_ec_t'
     nfm_func=nfm_funcs[nfm_func_name]
+    remove_world_pool = True
 
     env = GetCustomWorld(world_name, make_reflexive=True, state_repr=state_repr, state_enc=state_enc)
     env.redefine_nfm(nfm_func)
     env.capture_on_edges = edge_blocking
 
     # make task easier
-    env._remove_world_pool()
+    if remove_world_pool:
+        env._remove_world_pool()
     #env.redefine_goal_nodes([7])
     #env.current_entry=1
     #SimulateInteractiveMode(env)
     
     #Hyperparameters
     config={}
-    config['emb_dim']       = 64
+    config['emb_dim']       = 16
     config['emb_iter_T']    = 3
-    config['node_dim']      = 3
-    config['learning_rate'] = 0.005
+    config['node_dim']      = env.nfm.shape[1]
+    config['learning_rate'] = 0.0004
     config['gamma']         = 0.98
     config['lmbda']         = 0.95
-    config['eps_clip']      = 0.1
-    config['K_epoch']       = 3
+    config['eps_clip']      = 0.05
+    config['K_epoch']       = 5#3
     config['num_epi']       = 10000
-    seed_everything(0)
+    config['print_interval']= 20
+    config['plot_interval'] = 100
+    config['seed']          = 0
+    seed_everything(config['seed'])
 
-    logdir='runs/'+world_name+'/'+nfm_func_name+'_emb'+str(config['emb_dim'])+'_itT'+str(config['emb_iter_T'])+'_lr'+str(config['learning_rate'])+'_Kepoch'+str(config['K_epoch'] )
+    logdir='runs/'+world_name+'_wpremoved'+str(remove_world_pool)+'/'+nfm_func_name+'_emb'+str(config['emb_dim'])+'_itT'+str(config['emb_iter_T'])+'_lr'+str(config['learning_rate'])+'_Kepoch'+str(config['K_epoch'])+'_qmimic_maxreach_2L/SEED'+str(config['seed']) 
     model = GetTrainedModel(env, config, logdir)
